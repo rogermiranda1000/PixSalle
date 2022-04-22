ee52542245e6c22ee3f02c67faaf531b7ef614cb
roger.miranda <roger.miranda@students.salle.url.edu>
2022-04-22 18:23:47 +0200

    Init

---
+++ config/routing.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Salle\PixSalle\Controller\API\BlogAPIController;
+use Salle\PixSalle\Controller\SignUpController;
+use Salle\PixSalle\Controller\UserSessionController;
+use Slim\App;
+
+function addRoutes(App $app): void
+{
+    $app->get('/', UserSessionController::class . ':showSignInForm')->setName('signIn');
+    $app->get('/sign-in', UserSessionController::class . ':showSignInForm')->setName('signIn');
+    $app->post('/sign-in', UserSessionController::class . ':signIn');
+    $app->get('/sign-up', SignUpController::class . ':showSignUpForm')->setName('signUp');
+    $app->post('/sign-up', SignUpController::class . ':signUp');
+}
